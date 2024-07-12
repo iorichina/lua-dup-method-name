6dda5e6b7545b6f27a6b6ca19765ef7d97a13d3f
GitHub Action <action@github.com>
2024-07-12 09:02:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/iorichina/luadupmethodname/MyBundle.kt
+++ src/main/kotlin/com/github/iorichina/luadupmethodname/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.iorichina.luadupmethodname
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/iorichina/luadupmethodname/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/iorichina/luadupmethodname/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.iorichina.luadupmethodname.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
